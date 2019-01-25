73cc42f65ca629bdf67bc0f1226ce60acebe1c52
yixuan.chen <chenyixuan09@gmail.com>
2019-01-24 22:57:26 -0500

    Initial commit

---
+++ src/main/java/yixuan/project/webnotification/WebNotificationApplication.java
@@ -0,0 +1,14 @@
+package yixuan.project.webnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebNotificationApplication.class, args);
+	}
+
+}
+
